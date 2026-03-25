3d2f6fefad4650643328787f1fc9525140164f54
Cíntia Moreira <91138513+cintiamoreira@users.noreply.github.com>
2026-03-24 21:54:18 -0300

    adiciona redirecionamento da pagina principal

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import BotaoNavegacao from "@/components/BotaoNavegacao";
 import { temSessaoCookie } from "@/requests/usuarios";
-import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
@@ -12,7 +10,9 @@ export default function Home() {
   useEffect(() => {
     let ativo = true;
     void (async () => {
-      if (await temSessaoCookie()) {
+      if (!(await temSessaoCookie())) {
+        if (ativo) router.replace("/login");
+      } else {
         if (ativo) router.replace("/medidas");
       }
     })();
@@ -23,19 +23,7 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <BotaoNavegacao href="/login" texto="Ir para login" />
-        </div>
-      </main>
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start"></main>
     </div>
   );
 }
